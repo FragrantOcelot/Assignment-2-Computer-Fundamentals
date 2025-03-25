37c6b3ac7267a76e40c72d58baaefdf9090acf11
FragrantOcelot <amauryvekemans@gmail.com>
2025-03-25 19:05:39 +0400

    basic main file

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Main Module
+-----------
+Simple demonstration of the Royal Stay Hotel classes in action.
+"""
+
+from guest import Guest
+from loyalty_account import LoyaltyAccount
+from service_request import ServiceRequest
+from room import Room
+from booking import Booking
+from invoice import Invoice
+from payment import Payment
+from digital_payment import DigitalPayment
+from card_payment import CardPayment
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
